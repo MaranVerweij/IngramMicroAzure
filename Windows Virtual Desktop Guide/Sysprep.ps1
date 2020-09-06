da64b751038a599eed0b1b5b1b8bf635bff35b71
MaranVerweij <59657531+MaranVerweij@users.noreply.github.com>
2020-09-06 11:36:08 +0200

    Create Sysprep.ps1

---
+++ Windows Virtual Desktop Guide/Sysprep.ps1
@@ -0,0 +1,27 @@
+#This script will remove WVD agent software and disable Windows Update (and installer)
+#Author: Maran Verweij - Ingram Micro
+#Version: 1.0
+#No warranty implied
+
+#Run as Administrator
+
+#Uninstall WVD Agent software via Win32_Product class (Control Panel Software) in case MSI uninstallers missed a legacy version
+Try { 
+    $WVDSoftware = Get-WmiObject -Class Win32_Product | Where-Object{$_.Name -match "Agent Boot Loader" -or $_.Name -match "Remote Desktop Services" }
+    $WVDSoftware.Uninstall() 
+}
+Catch {
+    #Not installed, continue
+}
+
+#Disable Windows Update for future boot ups
+Set-Service wuauserv -StartupType Disabled
+
+#Start Windows Module Installer (as Sysprep requires it)
+Set-Service TrustedInstaller -Status Running -StartupType Automatic
+
+Start-Job -ScriptBlock { Start-Process -FilePath C:\Windows\System32\Sysprep\Sysprep.exe -ArgumentList ‘/generalize /oobe /shutdown’ }
+
+#Optional: Use at own risk. Implement the code below in a scheduled task to stop & disable Windows Modules Installer on startup. As of September 2020 Windows 10 enables
+#The Windows Module Installer even if it was disabled prior to running Sysprep. 
+#Set-Service TrustedInstaller -Status Stopped -StartupType Disabled
