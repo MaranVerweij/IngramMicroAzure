309e7878a2f7d9abdd7474757268c8411530fe51
MaranVerweij <59657531+MaranVerweij@users.noreply.github.com>
2021-01-07 16:55:18 +0100

    Update Create_SVP_WVD_andgrantpermissions.ps1

--- Windows Virtual Desktop Guide/Create_SVP_WVD_andgrantpermissions.ps1
+++ Windows Virtual Desktop Guide/Create_SVP_WVD_andgrantpermissions.ps1
@@ -1,20 +1,22 @@
 #This script creates an Azure Service Principal (Azure AD Application service principal) and assigns it permissions on the WVD tenant/workspace and specified Azure subscription.
 #Author: Maran Verweij - Ingram Micro
-#Version: 1.0
+#Version: 1.2
 #No warranty implied
 
 #Run as Administrator
 #Fill in the variables below
 
-$UserName = "you@companyname.onmicrosoft.com" #Azure account name in UPN format
-$Password = "password_here" #Password for Azure account
-$Azure_sub = "vvvvvvvvvv-01ca-abcd-8984-6645544" #Azure subscription ID
+$UserName = "" #Azure account name in UPN format
+$Password = "" #Password for Azure account
+$Azure_sub = "" #Azure subscription ID
 
-$SVP_Displayname = "SVP_WVD_1" #Display name of the service principal
-$SVP_Password = "password_here" #Specify service principal password, Max 850 characters, it is highly recommended to utilize the maximum for security concerns
-$Role_Scope = "/subscriptions/$Azure_sub" #Scope of service principal RBAC role assignment, leave default if unsure 
+$SVP_Displayname = "SVP_WVD_1" #Display name of the new service principal
+$SVP_Password = "" #Max 850 characters
 
 #No more manual input required as of this line, the script can be run now.
+
+$Role_Scope = "/subscriptions/$Azure_sub" #Scope of service principal RBAC role assignment, leave default if unsure 
+
 $Execution_Policy = Get-ExecutionPolicy #Get current Execution policy
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope CurrentUser -Force #Run this line seperately if ambigious module errors are thrown while running the script, then run the entire script again
 
@@ -44,12 +46,94 @@ $SVP = New-AzAdServicePrincipal -DisplayName $SVP_Displayname -PasswordCredentia
 
 Start-Sleep 10
 
-#Assign azure sub contributor permissions
-Try {
-New-AzRoleAssignment -RoleDefinitionName "Contributor" -ApplicationId $SVP.ApplicationId -Scope $Role_Scope -ErrorAction Stop
-}
-Catch {
-New-AzRoleAssignment -RoleDefinitionName "Contributor" -ApplicationId $SVP.ApplicationId
-}
+$JSON = 
+'{
+    "name": "WVD - Operational Maintenance",
+    "description": "Windows Virtual Desktop role for Operational Maintenance. Allows for: User session management and Session host management.",
+    "assignableScopes": [
+        "/subscriptions/PLACEHOLDER"
+    ],
+    "actions": [
+        "Microsoft.DesktopVirtualization/workspaces/providers/Microsoft.Insights/logDefinitions/read",
+        "Microsoft.DesktopVirtualization/workspaces/providers/Microsoft.Insights/diagnosticSettings/write",
+        "Microsoft.DesktopVirtualization/workspaces/providers/Microsoft.Insights/diagnosticSettings/read",
+        "Microsoft.DesktopVirtualization/workspaces/read",
+        "Microsoft.DesktopVirtualization/hostpools/sessionhosts/usersessions/*",
+        "Microsoft.DesktopVirtualization/hostpools/sessionhosts/delete",
+        "Microsoft.DesktopVirtualization/hostpools/sessionhosts/write",
+        "Microsoft.DesktopVirtualization/hostpools/sessionhosts/read",
+        "Microsoft.DesktopVirtualization/hostpools/providers/Microsoft.Insights/logDefinitions/read",
+        "Microsoft.DesktopVirtualization/hostpools/providers/Microsoft.Insights/diagnosticSettings/write",
+        "Microsoft.DesktopVirtualization/hostpools/providers/Microsoft.Insights/diagnosticSettings/read",
+        "Microsoft.DesktopVirtualization/hostpools/read",
+        "Microsoft.DesktopVirtualization/hostpools/write",
+        "Microsoft.DesktopVirtualization/register/action"
+    ],
+        "notActions": [],
+        "dataActions": [],
+        "notDataActions": []
+}'
 
-Write-Host "Document the following string, this is the Service Principal (Application) ID:" $SVP.ApplicationId
+$Adjusted_JSON = $JSON -replace ('PLACEHOLDER', $Azure_sub) 
+$Adjusted_JSON > WVDOperationalMaintenance.json 
+New-AzRoleDefinition -InputFile "WVDOperationalMaintenance.json"
+Remove-Item WVDOperationalMaintenance.json
+
+$JSON = 
+'{
+    "name": "VM and VMSS Operator",
+    "description": "Virtual Machine and Virtual Machine Scale Set Operator. Allows for redeploying, stopping, starting and deallocating for VMs and VMSS.",
+    "assignableScopes": [
+        "/subscriptions/PLACEHOLDER"
+    ],
+    "actions": [
+        "Microsoft.Compute/virtualMachines/deallocate/action",
+        "Microsoft.Compute/virtualMachines/restart/action",
+        "Microsoft.Compute/virtualMachines/redeploy/action",
+        "Microsoft.Compute/virtualMachines/powerOff/action",
+        "Microsoft.Compute/virtualMachines/start/action",
+        "Microsoft.Compute/virtualMachineScaleSets/virtualMachines/redeploy/action",
+        "Microsoft.Compute/virtualMachineScaleSets/virtualMachines/deallocate/action",
+        "Microsoft.Compute/virtualMachineScaleSets/virtualMachines/restart/action",
+        "Microsoft.Compute/virtualMachineScaleSets/virtualMachines/powerOff/action",
+        "Microsoft.Compute/virtualMachineScaleSets/virtualMachines/start/action",
+        "Microsoft.Compute/virtualMachineScaleSets/redeploy/action",
+        "Microsoft.Compute/virtualMachineScaleSets/start/action",
+        "Microsoft.Compute/virtualMachineScaleSets/restart/action",
+        "Microsoft.Compute/virtualMachineScaleSets/powerOff/action",
+        "Microsoft.Compute/virtualMachineScaleSets/deallocate/action",
+        "Microsoft.Compute/virtualMachines/read",
+        "Microsoft.Compute/virtualMachineScaleSets/virtualMachines/read",
+        "Microsoft.Compute/virtualMachineScaleSets/instanceView/read",
+        "Microsoft.Compute/virtualMachineScaleSets/virtualMachines/instanceView/read",
+        "Microsoft.Compute/virtualMachineScaleSets/read"
+    ],
+        "notActions": [],
+        "dataActions": [],
+        "notDataActions": []
+}'
+
+$Adjusted_JSON = $JSON -replace ('PLACEHOLDER', $Azure_sub) 
+$Adjusted_JSON > VMandVMSSOperator.json 
+New-AzRoleDefinition -InputFile "VMandVMSSOperator.json" 
+Remove-Item VMandVMSSOperator.json
+
+#Assign role: 'WVD - Operational Maintenance' to the Service Principal
+Try {
+    New-AzRoleAssignment -RoleDefinitionName "WVD - Operational Maintenance" -ApplicationId $SVP.ApplicationId -Scope $Role_Scope -ErrorAction Stop
+    }
+    Catch {
+    New-AzRoleAssignment -RoleDefinitionName "Contributor" -ApplicationId $SVP.ApplicationId
+    Write-Host "WARNING: Assigned Contributor role to the Service Principal because 'WVD - Operational Maintenance' could not be assigned. If you also received an error from 'New-AzRoleAssignment' the Contributor role could not be assigned either, most likely because the Service Principal could not be created."
+    }
+
+#Assign role: 'VM and VMSS Operator' to the Service Principal
+Try {
+    New-AzRoleAssignment -RoleDefinitionName "VM and VMSS Operator" -ApplicationId $SVP.ApplicationId -Scope $Role_Scope -ErrorAction Stop
+    }
+    Catch {
+    New-AzRoleAssignment -RoleDefinitionName "Contributor" -ApplicationId $SVP.ApplicationId
+    Write-Host "WARNING: Assigned Contributor role to the Service Principal because 'VM and VMSS Operator' could not be assigned. If you also received an error from 'New-AzRoleAssignment' the Contributor role could not be assigned either, most likely because the Service Principal could not be created."
+    }
+    
+Write-Host "Document the following string, this is the Service Principal (Application) ID:" $SVP.ApplicationId  
